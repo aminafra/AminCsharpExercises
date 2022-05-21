38019d3e475c13427b54e1dd856d7ec71c20877c
aminafra <mohamadamin.afrasiabi@gmail.com>
2022-05-22 01:51:08 +0430

    exercise two

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ExerciseTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int m = 5; m >= i; m--)
+                {
+                    Console.Write(" ");
+                }
+                for (int n = 1; n <= i; n++)
+                {
+                    Console.Write($"{n}");
+                }
+                for (int j = 1; j < i; j++)
+                {
+                    Console.Write($"{i-j}");
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
