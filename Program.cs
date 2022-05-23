f8b50b8b1ab5c4b705f721487b1343c4ddf9d02e
aminafra <mohamadamin.afrasiabi@gmail.com>
2022-05-23 21:41:48 +0430

    exersise Three

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExerciseThree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("enter as number:");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+            int j = 0;
+            for (int i = 1; i <= num; i += 2)
+            {
+                j += i;
+            }
+            Console.WriteLine(j);
+
+        }
+    }
+}
