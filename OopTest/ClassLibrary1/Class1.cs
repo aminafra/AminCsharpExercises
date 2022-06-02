595e43dd41429d8ac91afac46445cc60c13dcd89
aminafra <mohamadamin.afrasiabi@gmail.com>
2022-06-02 16:26:16 +0430

    oop exercise

---
+++ OopTest/ClassLibrary1/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace ClassLibrary1
+{
+    public class Salary
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _sPH;
+        private int _workingHours;
+
+        public int WorkingHours
+        {
+            get { return _workingHours; }
+            set { _workingHours = value; }
+        }
+
+        public int SPH
+        {
+            get { return _sPH; }
+            set { _sPH = value; }
+        }
+
+        public Salary(string name, int workingHours, int salaryPerHour)
+        {
+            this._name = name;
+            if (salaryPerHour == 0)
+            {
+                Console.WriteLine("boro baba");
+            }
+            else
+            {
+                this.SPH = salaryPerHour;
+            }
+            this.WorkingHours = workingHours;
+        }
+        public int Daily()
+        {
+            return SPH * _workingHours;
+        }
+        public int Monthly()
+        {
+            return SPH * _workingHours * 26;
+        }
+    }
+}
---
+++ OopTest/OopTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary1;
+
+namespace OopTest
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Salary> slryLst = new List<Salary>();
+
+            slryLst.Add(new Salary("amin", 8, 1000));
+            slryLst.Add(new Salary("ali", 10, 600));
+            slryLst.Add(new Salary("samin", 6, 700));
+            slryLst.Add(new Salary("farid", 11, 500));
+            slryLst.Add(new Salary("paniz", 7, 750));
+            Console.WriteLine("NAME --- WORKING HOURS -- SALARIES PER HOUR -- DAILY SALARY --- SALARY ");
+            foreach (var item in slryLst)
+            {
+                Console.WriteLine($" {item.Name}  -----  {item.WorkingHours}  -----------  {item.SPH}  ------------  {item.Daily()}  --------  {item.Monthly()}");
+            }
+        }
+    }
+}
