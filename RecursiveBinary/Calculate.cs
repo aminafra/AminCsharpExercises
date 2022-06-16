93d6cc4a4e798178cb859578901bda16057cac0b
aminafra <mohamadamin.afrasiabi@gmail.com>
2022-06-16 23:31:19 +0430

    decimal to binary with recursive

---
+++ RecursiveBinary/Calculate.cs
@@ -0,0 +1,19 @@
+﻿namespace RecursiveBinary;
+
+public class Calculate
+{
+    public int DecToBin(int dec)
+    {
+        int result;
+        if (dec != 0)
+        {
+            result = DecToBin(dec / 2) + (dec % 2);
+            Console.Write(result);
+            return 0;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
---
+++ RecursiveBinary/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RecursiveBinary;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("enter Decimal number:");
+        int dec = Convert.ToInt32(Console.ReadLine());
+
+        Calculate Binary = new Calculate();
+        Console.Write($"Binary of {dec} = ");
+        Binary.DecToBin(dec);
+        
+    }
+}
