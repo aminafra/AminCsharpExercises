44f5fd7aed1a7f898518fa0bfea2e7a0487f9851
aminafra <mohamadamin.afrasiabi@gmail.com>
2022-07-17 17:41:50 +0430

    exercises

---
+++ JsonExercise/Employees.cs
@@ -0,0 +1,13 @@
+﻿namespace JsonExercise;
+
+public class Employee
+{
+    public int id { get; set; }
+    public string first_name { get; set; }
+    public string last_name { get; set; }
+    public string email { get; set; }
+    public string gender { get; set; }
+    public int Age { get; set; }
+    public string ip_address { get; set; }
+}
+
---
+++ JsonExercise/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace JsonExercise;
+
+internal class Program : object
+{
+  public Program() : base()
+  {
+  }
+
+  public static void Main()
+  {
+    string json = File.ReadAllText(@"C:\Users\Aminsystem\source\repos\JsonExercise\Employee.json");
+
+    var ojbEmployee = JsonConvert.DeserializeObject<List<Employee>>(json);
+
+    var filtered = from employee in ojbEmployee
+      where employee.Age > 20 && employee.gender == "Male"
+      select employee;
+
+    Console.WriteLine($"total: {filtered.Count()} ");
+
+    filtered.ToList().ForEach(c=> Console.WriteLine($"ID: {c.id}, Full name: {c.first_name} {c.last_name}, Gender: {c.gender}\n"));
+  }
+}
